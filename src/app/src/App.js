1de3b94f3fd6bb6945ac2b41776796a5b7216387
hukuda222 <zionbanzai3@gmail.com>
2021-10-31 09:20:31 +0000

    dekita

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import allchars from './chars.js';
 
-function App() {
-  return (
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      name: ''
+    };
+    this.allchars = allchars.split("\n")
+    this.handleChange = this.handleChange.bind(this);    
+    this.genName = this.genName.bind(this);    
+  }
+ 
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }  
+  
+
+  genName() {
+    const index = Number(this.state.value);
+    const name1 = this.allchars[index%this.allchars.length];
+    const name2 = this.allchars[index*2%this.allchars.length];
+    const name3 = this.allchars[index*3%this.allchars.length];
+    const name4 = this.allchars[index*4%this.allchars.length];
+    this.setState({name: "Your name is "+name1+name2+" "+name3+name4});
+  }  
+   
+   render(){
+   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Aipp-header">
+         <h1>Perfect Name Generator</h1>
+	 <img src={logo} className="App-logo" alt="logo" />
       </header>
-    </div>
+        <div>
+	<p className="left"><h2>Please input my-number</h2></p> 
+	<textarea value={this.state.value} onChange={this.handleChange}  cols={20} rows={1} className="Input" />
+	</div>
+	  <br/>
+	<button onClick={this.genName} className="Button">
+            Generate
+        </button>
+         <p className="output"> 
+	 <h1>{this.state.name}</h1>
+	 </p>
+      </div>
   );
+  }
 }
 
 export default App;
