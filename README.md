e309db281b1d722e701005a891427a85d8d913df
藤田正悟 <zionbanzai3@gmail.com>
2021-10-31 20:56:21 +0900

    フォーマッタをかけた

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # name_generator
+マイナンバーから人名を生成する
+
+常用漢字一覧は下記のリポジトリから入手しました。
+https://github.com/rime-aca/character_set
--- src/app/src/App.js
+++ src/app/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import React from 'react'
 import allchars from './chars.js';
 
-class App extends React.Component{
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -11,44 +11,44 @@ class App extends React.Component{
       name: ''
     };
     this.allchars = allchars.split("\n")
-    this.handleChange = this.handleChange.bind(this);    
-    this.genName = this.genName.bind(this);    
+    this.handleChange = this.handleChange.bind(this);
+    this.genName = this.genName.bind(this);
   }
- 
+
   handleChange(event) {
-    this.setState({value: event.target.value});
-  }  
-  
+    this.setState({ value: event.target.value });
+  }
+
 
   genName() {
     const index = Number(this.state.value);
-    const name1 = this.allchars[index%this.allchars.length];
-    const name2 = this.allchars[index*2%this.allchars.length];
-    const name3 = this.allchars[index*3%this.allchars.length];
-    const name4 = this.allchars[index*4%this.allchars.length];
-    this.setState({name: "Your name is "+name1+name2+" "+name3+name4});
-  }  
-   
-   render(){
-   return (
-    <div className="App">
-      <header className="Aipp-header">
-         <h1>Perfect Name Generator</h1>
-	 <img src={logo} className="App-logo" alt="logo" />
-      </header>
+    const name1 = this.allchars[index % this.allchars.length];
+    const name2 = this.allchars[index * 2 % this.allchars.length];
+    const name3 = this.allchars[index * 3 % this.allchars.length];
+    const name4 = this.allchars[index * 4 % this.allchars.length];
+    this.setState({ name: "Your name is " + name1 + name2 + " " + name3 + name4 });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="Aipp-header">
+          <h1>Perfect Name Generator</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
         <div>
-	<p className="left"><h2>Please input my-number</h2></p> 
-	<textarea value={this.state.value} onChange={this.handleChange}  cols={20} rows={1} className="Input" />
-	</div>
-	  <br/>
-	<button onClick={this.genName} className="Button">
-            Generate
+          <p className="left"><h2>Please input my-number</h2></p>
+          <textarea value={this.state.value} onChange={this.handleChange} cols={20} rows={1} className="Input" />
+        </div>
+        <br />
+        <button onClick={this.genName} className="Button">
+          Generate
         </button>
-         <p className="output"> 
-	 <h1>{this.state.name}</h1>
-	 </p>
+        <p className="output">
+          <h1>{this.state.name}</h1>
+        </p>
       </div>
-  );
+    );
   }
 }
 
