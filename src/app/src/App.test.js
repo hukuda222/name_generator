6136f6ed1f7d7a66e97509a740a065f6fdb39b2c
藤田正悟 <zionbanzai3@gmail.com>
2021-11-07 18:39:21 +0900

    テスト通るようにした

--- src/app/src/App.test.js
+++ src/app/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from 'react';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/マイナンバー/i);
+  const linkElement = screen.getByText(/Perfect Name Generator/i);
   expect(linkElement).toBeInTheDocument();
 });
