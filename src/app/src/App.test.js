9ded33a616ae840c5065f9a9938f7133f2380b8d
藤田正悟 <zionbanzai3@gmail.com>
2021-11-07 18:21:46 +0900

    テスト通るようにした

--- src/app/src/App.test.js
+++ src/app/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/my-number/i);
+  const linkElement = screen.getByText(/マイナンバー/i);
   expect(linkElement).toBeInTheDocument();
 });
